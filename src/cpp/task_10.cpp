24558a122148cce74aa18165f65806b32628ad58
muhammad31032004 <127374110+muhammad31032004@users.noreply.github.com>
2024-01-14 20:39:53 +0500

    Update task_10.cpp

--- src/cpp/task_10.cpp
+++ src/cpp/task_10.cpp
@@ -1,9 +1,56 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Mukhammadkhon Asrorov
+ * Date: 14/01/2024
+ * Name: hw-10
  */
 
-class Problem10 {
-public:
-};
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+string generate3SATFormula(int numVariables, int numClauses) {
+    string formula;
+
+    srand(static_cast<unsigned int>(time(0)));
+
+    for (int i = 0; i < numClauses; ++i) {
+        string clause;
+
+        for (int j = 0; j < 3; ++j) {
+            char variable = 'A' + rand() % numVariables;
+            bool negate = rand() % 2 == 0;
+
+            if (negate) {
+                clause += "~";
+            }
+
+            clause += variable;
+
+            if (j < 2) {
+                clause += " | ";
+            }
+        }
+
+        formula += "(" + clause + ")";
+
+        if (i < numClauses - 1) {
+            formula += " & ";
+        }
+    }
+
+    return formula;
+}
+
+int main() {
+    int numVariables = 3;
+    int numClauses = 2;
+  
+    string formula = generate3SATFormula(numVariables, numClauses);
+
+    cout << "Generated 3-SAT formula: " << formula << endl;
+
+    return 0;
+}
