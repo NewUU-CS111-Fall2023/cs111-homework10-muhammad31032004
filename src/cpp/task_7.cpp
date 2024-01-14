21483c3b97b6abf81cbea1e014221869403280bc
muhammad31032004 <127374110+muhammad31032004@users.noreply.github.com>
2024-01-14 20:34:22 +0500

    Update task_7.cpp

--- src/cpp/task_7.cpp
+++ src/cpp/task_7.cpp
@@ -1,9 +1,20 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Mukhammadkhon Asrorov
+ * Date: 14/01/2024
+ * Name: hw-10
  */
 
 class Problem7 {
 public:
+    int numRabbits(vector<int>& answers) {
+        int ans = 0;
+        unordered_map<int,int> mp;
+        for(auto &i: answers){
+            mp[i]++;
+        }
+        for(auto &i: mp){
+            ans += ceil((float)i.second/(i.first+1))*(i.first+1);
+        }
+        return ans;
+    }
 };
