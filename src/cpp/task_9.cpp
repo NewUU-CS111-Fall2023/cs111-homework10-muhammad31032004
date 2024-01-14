1842be05c4d9496f63acedcb0463c46c2d7b555e
muhammad31032004 <127374110+muhammad31032004@users.noreply.github.com>
2024-01-14 20:37:53 +0500

    Update task_9.cpp

--- src/cpp/task_9.cpp
+++ src/cpp/task_9.cpp
@@ -1,9 +1,50 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Mukhammadkhon Asrorov
+ * Date: 14/01/2024
+ * Name: hw-10
  */
 
-class Problem9 {
-public:
-};
+#include <iostream>
+#include <unordered_map>
+#include <stack>
+
+using namespace std;
+
+bool evaluateFormula(const string& formula, const unordered_map<char, bool>& assignment) {
+    stack<bool> resultStack;
+
+    for (char ch : formula) {
+        if (isalpha(ch)) {
+            resultStack.push(assignment.at(ch));
+        } else if (ch == '~') {
+            bool operand = resultStack.top();
+            resultStack.pop();
+            resultStack.push(!operand);
+        } else if (ch == '|') {
+            bool operand2 = resultStack.top();
+            resultStack.pop();
+            bool operand1 = resultStack.top();
+            resultStack.pop();
+            resultStack.push(operand1 || operand2);
+        } else if (ch == '&') {
+            bool operand2 = resultStack.top();
+            resultStack.pop();
+            bool operand1 = resultStack.top();
+            resultStack.pop();
+            resultStack.push(operand1 && operand2);
+        }
+    }
+
+    return resultStack.top();
+}
+
+int main() {
+    string formula = "(A | ~B) & (~A | B | C)";
+    unordered_map<char, bool> assignment = {{'A', true}, {'B', false}, {'C', true}};
+
+    bool result = evaluateFormula(formula, assignment);
+
+    cout << (result ? "Satisfied" : "Not Satisfied") << endl;
+
+    return 0;
+}
